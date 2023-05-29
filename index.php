b9d310cf9aaf09cc6715e67f1855485632f59285
@Edwarcorredor EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-05-29 17:18:27 -0500

    feat: :sparkles: tipos de datos
    Se agg los tipos de datos con su explicación

--- index.php
+++ index.php
@@ -29,6 +29,37 @@
     $nombre = (string) "Edwar";
     /**
      * *$variable = (tipo) se usa para definir variables y decir estrictamente su tipo de dato
-    */ 
+    */
+    
+    $logueado = true;
+    var_dump($logueado);
 
+    /**
+     * *Booleanos (bool): se utilizan para almacenar valores de    verdad o falsedad, que se representan por true o false  
+    */
+
+    $numero = 200;
+    var_dump($numero);
+    /**
+     * *Enteros (int): se utilizan para almacenar números enteros sin decimales
+    */
+
+    $float = 200.5;
+    var_dump($float);
+    /**
+     * *Punto flotante (float): se utilizan para almacenar números con decimales.
+    */
+
+    $nombre = "Edwar";
+    var_dump($nombre);
+    /**
+     * *Cadenas de texto (string): se utilizan para almacenar texto y caracteres.
+    */
+
+    $array = [];
+    var_dump($array);
+    /**
+     * *Arreglos (array): se utilizan para almacenar una colección de datos, que pueden ser de diferentes tipos.
+
+    */
 ?>
