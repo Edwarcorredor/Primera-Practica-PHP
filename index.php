46188ded5a403416e63fe582bec8a4ef26f78b86
@Edwarcorredor EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-05-29 17:07:17 -0500

    docs: :art: Cambiar orden documentacion
    Se distribuye debajo de cada codigo la documentacion

--- index.php
+++ index.php
@@ -1,21 +1,34 @@
 <?php
   /**
-   **echo imprime contenido
-   **$variable se usa para definir variables
-   **printf otra forma de imprimir contenido usando un tipo de template string de js
-   **sprintf Se comporta parecido a un template string
-   **var_dump se usa para imprimir el contenido y la estructura del dato
-   **$variable = (tipo) se usa para definir variables y decir estrictamente su tipo de dato
    **Se tiene que dividir por carpetas los tipos de archivos para tener un orden en el proyecto
   */
   
-    echo 5; 
+    echo 5;
+    /**
+     * * echo imprime contenido
+    */ 
     $texto = "Mundo ";
-    printf("Hola %s ", $texto); 
+    printf("Hola %s ", $texto);
+    /**
+     * *$variable se usa para definir variables
+     * *printf otra forma de imprimir contenido usando un tipo de template string de js
+    */ 
     $mensaje = sprintf("Hola %s ", $texto);
+    /**
+     **sprintf Se comporta parecido a un template string 
+    */ 
     define("MENSAJE","mundo");
     printf("%s %s ",$mensaje, MENSAJE);
+    /**
+     **define se usa para crear constantes 
+    */
     var_dump($mensaje);
+    /**
+     * *var_dump se usa para imprimir el contenido y la estructura del dato
+    */
     $nombre = (string) "Edwar";
+    /**
+     * *$variable = (tipo) se usa para definir variables y decir estrictamente su tipo de dato
+    */ 
 
 ?>
