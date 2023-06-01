114f88aaf49b88fe2590adb498582d2ae226d6e3
@Edwarcorredor EdwarDaniloCorredorAmayaCAMPUS <eddacoam7.2@gmail.com>
2023-06-01 16:56:00 -0500

    docs: :art: arreglos

--- index.php
+++ index.php
@@ -5,136 +5,216 @@
   /**
    * TODO: 6 Funciones de salida
   */
-    echo 5;
-    /**
-     * * echo imprime contenido
-    */ 
-    $texto = "Mundo ";
-    printf("Hola %s ", $texto);
-    /**
-     * *$variable se usa para definir variables
-     * *printf otra forma de imprimir contenido usando un tipo de template string de js
-    */ 
-    $mensaje = sprintf("Hola %s ", $texto);
-    /**
-     **sprintf Se comporta parecido a un template string 
-    */ 
-    var_dump($mensaje);
-    /**
-     * *var_dump se usa para imprimir el contenido y la estructura del dato
-    */
-    /**
-     *TODO:7 Variables y constantes 
-    */
+  echo 5;
+  /**
+   * * echo imprime contenido
+  */ 
+  $texto = "Mundo ";
+  printf("Hola %s ", $texto);
+  /**
+   * *$variable se usa para definir variables
+   * *printf otra forma de imprimir contenido usando un tipo de template string de js
+  */ 
+  $mensaje = sprintf("Hola %s ", $texto);
+  /**
+   **sprintf Se comporta parecido a un template string 
+  */ 
+  var_dump($mensaje);
+  /**
+   * *var_dump se usa para imprimir el contenido y la estructura del dato
+  */
+  /**
+   *TODO:7 Variables y constantes 
+  */
 
-    $nombre = (string) "Edwar";
-    /**
-     * *$variable = (tipo) se usa para definir variables y decir estrictamente su tipo de dato
-    */
+  $nombre = (string) "Edwar";
+  /**
+   * *$variable = (tipo) se usa para definir variables y decir estrictamente su tipo de dato
+  */
 
-    define("MENSAJE","mundo");
-    printf("%s %s ",$mensaje, MENSAJE);
-    /**
-     **define se usa para crear constantes 
-    */
-    
-    /**
-     *TODO:8 Tipos de datos
-    */
+  define("MENSAJE","mundo");
+  printf("%s %s ",$mensaje, MENSAJE);
+  /**
+   **define se usa para crear constantes 
+  */
+  
+  /**
+   *TODO:8 Tipos de datos
+  */
 
-    $logueado = true;
-    var_dump($logueado);
+  $logueado = true;
+  var_dump($logueado);
 
-    /**
-     * *Booleanos (bool): se utilizan para almacenar valores de    verdad o falsedad, que se representan por true o false  
-    */
+  /**
+   * *Booleanos (bool): se utilizan para almacenar valores de    verdad o falsedad, que se representan por true o false  
+  */
 
-    $numero = 200;
-    var_dump($numero);
-    /**
-     * *Enteros (int): se utilizan para almacenar números enteros sin decimales
-    */
+  $numero = 200;
+  var_dump($numero);
+  /**
+   * *Enteros (int): se utilizan para almacenar números enteros sin decimales
+  */
 
-    $float = 200.5;
-    var_dump($float);
-    /**
-     * *Punto flotante (float): se utilizan para almacenar números con decimales.
-    */
+  $float = 200.5;
+  var_dump($float);
+  /**
+   * *Punto flotante (float): se utilizan para almacenar números con decimales.
+  */
 
-    $nombre = "Edwar";
-    var_dump($nombre);
-    /**
-     * *Cadenas de texto (string): se utilizan para almacenar texto y caracteres.
-    */
+  $nombre = "Edwar";
+  var_dump($nombre);
+  /**
+   * *Cadenas de texto (string): se utilizan para almacenar texto y caracteres.
+  */
 
-    $array = [];
-    var_dump($array);
-    /**
-     * *Arreglos (array): se utilizan para almacenar una colección de datos, que pueden ser de diferentes tipos.
+  $array = [];
+  var_dump($array);
+  /**
+   * *Arreglos (array): se utilizan para almacenar una colección de datos, que pueden ser de diferentes tipos.
 
-    */
+  */
 
-    /**
-     * TODO: Números y operadores
-     * 
-     * todo : Operadores aritmeticos 
-     * * + = Adicion
-     * * - = Substraccion
-     * * * = Multiplicacion
-     * * / = Divison
-     * * % = Modulo
-     * * (**) = Exponenciacion
-     * 
-     * todo: Operadores de asignacion
-     * * = igualar
-     * 
-     * todo: Operadores de Array
-     * * + = Union
-     * * == = Igualdad
-     * * === = Identico
-     * * != = desigualdad
-     * * <> = desigualdad
-     * * !== = no es identico
-     * 
-     * todo: Operadores Bitwise
-     * * & = and
-     * * ^ = xor
-     * * | = not
-     * * << = shift left
-     * * >> = shift rigth
-     * 
-     * todo: Operadores de comparacion
-     * * == = igual
-     * * === = identico
-     * * != = no es igual
-     * * !== = no es identico
-     * * <> = no igual
-     * * < = menor a esto
-     * * <= menor o igual
-     * * > = mayor a esto
-     * * >= mayor o igual
-     * * <=> = spaceship 
-     * 
-     * todo: Operadores de ejecucion
-     * * `` = backticks
-     * 
-     * todo: Operadores para control de errores
-     * * @ = at sign
-     * 
-     * todo: Operadores de incremento/decremento
-     * * ++$a = preIncremento
-     * * $a++ = PostIncremento
-     * * --$a = PreDrecemento
-     * * $a-- = PostDecremento
-     * 
-     * todo: Operadores de logica
-     * * && = and
-     * * || = or 
-     * * ! = not
-     * * and = and
-     * * xor = xor
-     * * or = or
-     *
-    */
+  /**
+   * TODO: Números y operadores
+   * 
+   * todo : Operadores aritmeticos 
+   * * + = Adicion
+   * * - = Substraccion
+   * * * = Multiplicacion
+   * * / = Divison
+   * * % = Modulo
+   * * (**) = Exponenciacion
+   * 
+   * todo: Operadores de asignacion
+   * * = igualar
+   * 
+   * todo: Operadores de Array
+   * * + = Union
+   * * == = Igualdad
+   * * === = Identico
+   * * != = desigualdad
+   * * <> = desigualdad
+   * * !== = no es identico
+   * 
+   * todo: Operadores Bitwise
+   * * & = and
+   * * ^ = xor
+   * * | = not
+   * * << = shift left
+   * * >> = shift rigth
+   * 
+   * todo: Operadores de comparacion
+   * * == = igual
+   * * === = identico
+   * * != = no es igual
+   * * !== = no es identico
+   * * <> = no igual
+   * * < = menor a esto
+   * * <= menor o igual
+   * * > = mayor a esto
+   * * >= mayor o igual
+   * * <=> = spaceship (-1 si izquierda es menor, 0 si es igual, 1 si izquierda es mayor)
+   * 
+   * todo: Operadores de ejecucion
+   * * `` = backticks
+   * 
+   * todo: Operadores para control de errores
+   * * @ = at sign
+   * 
+   * todo: Operadores de incremento/decremento
+   * * ++$a = preIncremento
+   * * $a++ = PostIncremento
+   * * --$a = PreDrecemento
+   * * $a-- = PostDecremento
+   * 
+   * todo: Operadores de logica
+   * * && = and
+   * * || = or 
+   * * ! = not
+   * * and = and
+   * * xor = xor
+   * * or = or
+   *
+  */
 
-?>
+  $numero1 = 20;
+  $numero2 = 30;
+  $numero3 = 30;
+  $numero4 = "30";
+
+  var_dump($numero1 > $numero2);
+  echo "<br/>";
+
+  var_dump($numero1 < $numero2);
+  echo "<br/>";
+
+  var_dump($numero1 >= $numero2);
+  echo "<br/>";
+
+  var_dump($numero1 <= $numero2);
+  echo "<br/>";
+
+  var_dump($numero2 == $numero3);
+  echo "<br/>";
+
+  var_dump($numero2 == $numero4);
+  echo "<br/>";
+
+  var_dump($numero2 === $numero4);
+  echo "<br/>";
+
+  var_dump($numero1 <=> $numero2);
+  echo "<br/>";
+
+  var_dump($numero2 <=> $numero3);
+  echo "<br/>";
+
+  var_dump($numero2 <=> $numero1);
+  echo "<br/>";
+
+
+  $nombreCliente = "Campers Campuslands";
+
+  echo strlen($nombreCliente);
+  var_dump($nombreCliente);
+
+  /**
+   ** Conocer extension de un string
+   * @var $nombreCliente 
+  */
+
+  $text = trim($nombreCliente);
+  echo strlen($texto);
+
+  /**
+   **Eliminar espacios en blanco 
+  */
+
+  echo strtoupper($nombreCliente);
+
+  /**
+   **Convertirlo a mayuscula 
+  */
+
+  echo strtolower($nombreCliente);
+  /**
+   **Convertirlo en minuscula 
+  */
+
+  $mail1 = "correo@correo.com";
+  $mail2 = "correo@correo.com";
+
+  var_dump(strtolower($mail1) === strtolower($mail2));
+  echo str_replace('Juan','J',$nombreCliente);
+
+  echo strpos($nombreCliente,'Pedro');
+  /**
+   **Revisar si un string existe o no
+  */
+
+  $tipoCliente = "Premium - Empresarial";
+
+  echo "<br>";
+
+  echo "El Cliente". $nombreCliente. "es". $tipoCliente;
+  
